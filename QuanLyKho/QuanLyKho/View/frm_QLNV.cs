bfcc50cdbc3dd269ec6dc20076a111e8efc4c75d
Vungocmta97 <vungocmta97@gmail.com>
2018-04-20 09:19:58 +0700

    Init Project

---
+++ QuanLyKho/QuanLyKho/View/frm_QLNV.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QuanLyKho.Object;
+using QuanLyKho.Control;
+namespace QuanLyKho.View
+{
+    public partial class frm_QLNV : Form
+    {
+      
+        public frm_QLNV()
+        {
+            InitializeComponent();
+        }
+
+     
+    }
+}
