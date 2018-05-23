910cbb259b64a6b84517d87336e0758124f50098
tuanpanda <dxtuan1997@gmail.com>
2018-05-23 12:19:58 +0700

    from thống kê hàng

--- QuanLyKho/QuanLyKho/View/frmThongKeHangHoa.cs
+++ QuanLyKho/QuanLyKho/View/frmThongKeHangHoa.cs
@@ -13,12 +13,26 @@ namespace QuanLyKho.View
 {
     public partial class frmThongKeHangHoa : Form
     {
-
+        ThongKeCtrlH tke = new ThongKeCtrlH();
+        DataTable dt = new DataTable();
         public frmThongKeHangHoa()
         {
             InitializeComponent();
         }
 
-     
+        private void frmThongKeHangHoa_Load(object sender, EventArgs e)
+        {
+            dt = tke.GetData("select * from sanpham");
+            dgvHangHoa.DataSource = dt;
+        }
+
+        private void txtTimKiem_TextChanged(object sender, EventArgs e)
+        {
+            if (dt.Rows.Count > 0)
+            {
+                string str = string.Format("MaSP like '%{0}%' or TenSP like '%{1}%' or ChungLoai like '%{2}%'", txtTimKiem.Text, txtTimKiem.Text, txtTimKiem.Text);
+                dt.DefaultView.RowFilter = str;
+            }
+        }
     }
 }
